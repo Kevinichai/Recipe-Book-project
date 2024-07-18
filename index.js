51f7c920143b83d3b4f7aa0f4bb43fc14290df55
kevinkamau <kevin.kamau2@student.moringaschool.com>
2024-07-18 23:35:34 +0300

    completed styling and fixed a few errors in Javascript

--- index.js
+++ index.js
@@ -1,32 +1,73 @@
 //Joininng HTML IDS to subsequent javascriptvariables
-const mealImage = document.getElementById('mealImage')
-const recipeTitle = document.getElementById('recipe-title')
-const mealDetails = document.getElementById('mealDetails')
-const mealContainer=document.getElementById('mealContainer')
-const searchBtn= document.querySelector('.searchBtn')
-const inputField=document.getElementById('input')
-const selectBtn=document.getElementById('SelectBtn')
-const recipeDetails=document.getElementById('recipe-details')
+const mealImage = document.getElementById('mealImage');
+const mealTitle = document.getElementById('recipe-title');
+const mealInstructions = document.getElementById('mealInstructions');
+const mealContainer=document.getElementById('mealContainer');
+const searchBtn= document.querySelector('.searchBtn');
+const inputField=document.getElementById('input');
+const selectBtn=document.getElementById('SelectBtn');
+const mealList = document.getElementById('mealList');
 
 
 //Declaring api source
-const allMealsurl=`www.themealdb.com/api/json/v1/1/search.php?s=${meals}`
+const allMealsurl='https://www.themealdb.com/api/json/v1/1/search.php?s=';
 
-fetchMeals();
-//function to fect api data
-function fetchMeals() {
-fetch(allMealsurl)
+//function to fect api data based on query
+function fetchMeals(query) {
+
+fetch(`${allMealsurl}${query}`)
 .then(response => response.json())
+.then(data=> {
+    if (data.meals) {
+        displayMeal(data.meals[0]);
+        displayMealList(data.meals);
+    } else {alert('Meal Unavailable');
 
+    }
+})
+.catch(error => console.error('Unable to fetch meals',error));
 }
 
+//Function to fetch a random meal(nipike button)
+function fetchRandomMeal() {
+    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+    .then(response =>response.json())
+    .then(data =>{displayMeal(data.meals[0]);
+    })
+    .catch(error => console.error('Unable to locate random recipe',error));
+}
+//Function to display Meals
+function displayMeal(meal){
+mealImage.src = meal.strMealThumb;
+mealTitle.textContent = meal.strMeal;
+mealInstructions.textContent = meal.strMealInstructions;
+mealContainer.textContent=meal.idMeal
 
+}
+//Function to configure the mealList
+function displayMealList(meals) {
+mealList.innerHTML=''
+meals.forEach(meal => {
+    const mealItem = document.createElement('li')
+    mealItem.textContent=meal.strMeal;
+    mealList.appendChild(mealItem);
 
-//Adding event listener to buttons
-searchBtn.addEventListener('click', () => {
-    const newItem = itemInserted.value.trim();
-    if (newItem) {
-        addItem(newItem);
-        itemInserted.value = '';
-    }
+    //Adding an eventlistener to meal items
+    mealItem.addEventListener('click', () => {
+        displayMeal(meal);
+    });
 });
+}
+//Adding event listener to buttons
+searchBtn.addEventListener('click', (event) => {
+   event.preventDefault();
+   const query =inputField.value.trim();
+   if(query){
+    fetchMeals(query);
+    inputField.value='';
+   }
+    });
+
+//Adding event listener to nipike button
+selectBtn.addEventListener('click',() => {fetchRandomMeal();
+})
